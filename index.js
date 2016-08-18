ea058472fa503577ac3d4b667714f5838576f347
Henri Chabrand <henri.chabrand@gmail.com>
2016-08-18 14:51:24 +0200

    api.ai

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 var express = require('express')
 var app = express()
 
+
+var apiai = require('apiai');
+ 
+var app = apiai("798d7a884de94566872e37c9959f1712");
+ 
+var request = app.textRequest('Give me th weather in Paris');
+
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
 
-  response.send('Hello Great Worldfff!');
-console.log("Node app is running at localhost:");
+	 
+	 
+	request.on('response', function(response) {
+	    console.log(response);
+	});
+	 
+	request.on('error', function(error) {
+	    console.log(error);
+	});
+	 
+	request.end()
 })
 
 app.listen(app.get('port'), function() {
