e5b3a6e6b8329d6645b9a30dc8fc47b1fbe2624c
HenriChabrand <HenriChabrand@users.noreply.github.com>
2016-08-18 11:57:19 +0200

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,8 @@
-// Call the console.log function.
-console.log("Hello World");
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(8080);
+
+console.log('Server started');
