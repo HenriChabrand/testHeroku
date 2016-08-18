a24c1301e032dc17f661cc64aea565b3a234e8f6
Henri Chabrand <henri.chabrand@gmail.com>
2016-08-18 16:11:45 +0200

    fix

--- index.js
+++ index.js
@@ -1,56 +1,18 @@
 
 
-'use strict';
-
 // var apiai = require("../module/apiai");
 var apiai = require("apiai")
 
 var app = apiai("22db42aa367c4d3b9db67fdf3562c0e0");
 
-var sessionId = "henriwhattestisthat";
+var request = app.textRequest('give me the weather');
 
-var user_entities = [{
-    name: 'Application',
-    sessionId: sessionId,
-    extend: false,
-    entries: [
-        {
-            value: 'Firefox',
-            synonims: ['Firefox']
-        },
-        {
-            value: 'XCode',
-            synonims: ['XCode']
-        }, 
-        {
-            value: 'Sublime Text',
-            synonims: ['Sublime Text']
-        }
-    ]
-}];
-
-var user_entities_request = app.userEntitiesRequest(user_entities);
-
-user_entities_request.on('response', function(response) {
-    console.log('User entities response: ');
+request.on('response', function(response) {
     console.log(response);
-
-    var request = app.textRequest('Open application Firefox');
-
-    request.on('response', function(response) {
-        console.log('Query response: ');
-        console.log(response);
-    });
-
-    request.on('error', function(error) {
-        console.log(error);
-    });
-
-    request.end()
 });
 
-user_entities_request.on('error', function(error) {
+request.on('error', function(error) {
     console.log(error);
 });
 
-user_entities_request.end()
+request.end();
