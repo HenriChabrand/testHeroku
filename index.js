4a940ad56eda1df088e014ab2da414f8c2fc574c
Henri Chabrand <henri.chabrand@gmail.com>
2016-08-18 15:21:47 +0200

    fix token

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var apiai = require('apiai');
 
-var app = apiai("<your client access token>");
+var app = apiai("798d7a884de94566872e37c9959f1712");
 
-var request = app.textRequest('<Your text query>');
+var request = app.textRequest('Give me the weather in Paris');
 
 request.on('response', function(response) {
     console.log(response);
